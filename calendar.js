e297dad11ad3a9f89f5a1de5697965eb4a327dfd
Kyle Lortscher <kylelortscher@gmail.com>
2015-07-10 11:51:41 -0700

    Create calevent Mongo Collection

--- calendar.js
+++ calendar.js
@@ -1,13 +1,26 @@
+CalEvent = new Mongo.Collection('calevent');
+
 if (Meteor.isClient) {
   Template.main.rendered = function(){
     var calendar = $('#calendar').fullCalendar({
-      
+      dayClick:function(date,allDay,jsEvent,view){
+        var calendarEvent = {};
+        calendarEvent.start = date;
+        calendarEvent.end = date;
+        calendarEvent.title = 'New Event';
+        calendarEvent.owner = Meteor.userId;
+        Meteor.call('saveCalEvent', calendarEvent);
+      }
     })
   }
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    Meteor.methods({
+      'saveCalEvent':function(ce){
+        CalEvent.insert(ce)
+      }
+    })
   });
 }
